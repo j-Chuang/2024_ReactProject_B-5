40ebbfea1787fd6850a9d87459c3859613b038e5
chuang <haroowaaku666@gmail.com>
2025-02-11 19:53:20 +0800

    Plus vite,axios,sass,basicBS

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.jsx'
+import 'bootstrap';
+// import './index.css'
+import './assets/all.scss'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
